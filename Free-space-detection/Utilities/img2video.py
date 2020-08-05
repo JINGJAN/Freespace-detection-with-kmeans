5e3371fce2e82c73b4e4964a460fef2911b0adf9
JINGJAN <38492507+JINGJAN@users.noreply.github.com>
2020-08-05 16:50:50 +0200

    Add files via upload
    image to video

---
+++ Free-space-detection/Utilities/img2video.py
@@ -0,0 +1,26 @@
+import cv2
+import os
+
+
+
+imgarray = []
+# path = "/Users/nebel/Desktop/20200611_1/20200611_1c"
+path = "../RAW_data/josn_file_foto"
+listofimg = os.listdir(path)
+sampleimg = cv2.imread(path + "/"+ listofimg[0])
+
+#get the w h c from sampleimage
+width,height,channel = sampleimg.shape
+size = (height,width)
+print(width,height,channel)
+out = cv2.VideoWriter("/Users/nebel/Desktop/out_evening.avi",cv2.VideoWriter_fourcc('M','J','P','G'),30,size)
+
+#loop the image in range 1000,store the image
+for i in range(399):
+    imgpath = path + "/c" + str(i+1)+".jpg"
+    print(imgpath)
+    cv2.imshow("show",cv2.imread(imgpath))
+    cv2.waitKey(10)
+    out.write(cv2.imread(imgpath))
+# print(len(imgarray))
+out.release()
